5bee6151beb180f38c1297e3ff3f09b7cff8ad8b
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-21 19:23:30 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,5 @@
 const sinon = require( 'sinon' )
 
-// describe('index.js', function () {
-//   describe('companyName', function () {
-//     it('is set as Scuber', function () {
-//       expect(companyName).to.equal('Scuber');
-//     });
-
-//     it('is defined as a const', function () {
-//       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
-//     });
-//   });
-
-  describe('mostProfitableNeighborhood', function () {
-    it('is declared as equal to Chelsea', function () {
-      expect(mostProfitableNeighborhood).to.equal('Chelsea');
-    });
-
-    it('does not raise error if the mostProfitableNeighborhood is changed', function () {
-      expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
-    });
-  });
 
   describe('companyCeo', function () {
     it('is declared as equal to Susan Smith', function () {
